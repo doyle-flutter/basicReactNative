f035ad1b8a8b782cef6047b87dbbcbbef41292e0
James-dev <56661529+doyle-flutter@users.noreply.github.com>
2020-11-25 10:57:41 +0900

    Create mainPage.js

---
+++ detachAndState/pages/mainPage.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import {View, Text} from 'react-native';
+
+class MainPage extends Component{
+    
+    render(){
+        return (
+            <View>
+                <Text>안녕하세요 :)</Text>
+            </View>
+        );
+    }
+}
+
+
+export default MainPage;
