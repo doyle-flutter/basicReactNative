38cbf5cb62a346e613d0c539969848d92ba757f6
James-dev <56661529+doyle-flutter@users.noreply.github.com>
2020-11-25 19:37:03 +0900

    Update stateMainPage.js

--- detachAndState/pages/stateMainPage.js
+++ detachAndState/pages/stateMainPage.js
@@ -2,6 +2,11 @@ import React, {Component} from 'react';
 import {View, Text, Button} from 'react-native';
 
 class MainPage extends Component{
+    
+    constructor(props){
+        super(props);
+        this.state = {viewTxt : ''};
+    }
 
     // LifeCycle 
     componentWillMount(){
