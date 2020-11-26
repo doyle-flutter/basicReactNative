6a94f81d47350161232d277357f2e1a6d8271b6f
James-dev <56661529+doyle-flutter@users.noreply.github.com>
2020-11-26 17:03:27 +0900

    Update App.js

--- navigation/App.js
+++ navigation/App.js
@@ -7,7 +7,6 @@
 // @ install [ terminal ]
 // > npm i @react-navigation/native
 // > expo install react-native-gesture-handler react-native-reanimated react-native-screens react-native-safe-area-context @react-native-community/masked-view
-// > pod install
 // > npm install react-native-reanimated react-native-gesture-handler react-native-screens react-native-safe-area-context @react-native-community/masked-view
 // > npx pod-install ios
 
