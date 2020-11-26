060ee7d6324b09fd9284591f2426001c86e33e41
James-dev <56661529+doyle-flutter@users.noreply.github.com>
2020-11-26 14:24:50 +0900

    Create mainPage.js

---
+++ navigation/pages/mainPage.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react';
+import {View, Text, Button} from 'react-native';
+
+class MainPage extends Component{
+
+    constructor(props){
+        super(props);
+        this.state = {viewTxt : ''};
+    }
+
+    componentWillMount(){
+        this.setState({
+            viewTxt : '안녕하세요'
+        });
+    }
+    
+    render(){
+        return (
+            <View>
+                <Text>{this.state.viewTxt}</Text>
+                <Button onPress={() => this._clickBtn()} title={"Click"}></Button>
+            </View>
+        );
+    }
+    _clickBtn(){
+        this.setState({
+            viewTxt: this.state.viewTxt+=1
+        });
+        return;
+    }
+}
+
+
+export default MainPage;
