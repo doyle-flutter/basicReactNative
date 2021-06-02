17ef00086fda42801e30f653501e730c619dd0a4
James-dev <56661529+doyle-flutter@users.noreply.github.com>
2021-06-03 00:05:18 +0900

    Create functions.js

---
+++ example/03/functions.js
@@ -0,0 +1,54 @@
+// firebase login
+// firebase init
+// firebase deploy
+
+// -- index.js --
+
+const functions = require("firebase-functions");
+
+exports.helloWorld = functions.https.onRequest((request, response) => {
+  functions.logger.info("Hello logs!", {structuredData: true});
+  response.json({name:'jamesdev'});
+});
+
+exports.expo = functions.https.onRequest(async (request, response) => {
+    if(request.method != "POST") return response.json(false);
+    const { Expo } = require('expo-server-sdk'),
+    t = "",
+    expo = new Expo({accessToken: t});
+    const handlePushTokens = async (savedPushTokens) => {
+        let notifications = [];
+        for (let pushToken of savedPushTokens) {
+          if (!Expo.isExpoPushToken(pushToken)) {
+            console.error(`Push token ${pushToken} is not a valid Expo push token`);
+            continue;
+          }
+      
+          notifications.push({
+            to: pushToken,
+            sound: "default",
+            title: "title",
+            body: "body",
+            data: { body: "body" }
+          });
+        }
+      
+        let chunks = expo.chunkPushNotifications(notifications);
+      
+        return (async () => {
+          for (let chunk of chunks) {
+            try {
+              let receipts = await expo.sendPushNotificationsAsync(chunk);
+              console.log(receipts);
+            } catch (error) {
+              console.error(error);
+            }
+          }
+        })();
+      };
+    const {data, expoPushToken} = request.body,
+      expoToken = expoPushToken.data;
+    console.log(expoToken);
+    await handlePushTokens([expoToken]);
+    response.json(true);
+  });
